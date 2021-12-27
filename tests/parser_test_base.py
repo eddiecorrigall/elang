0046e3b7167d1615356c73b1a9d11478551b75d7
Eddie Corrigall <edward.corrigall@clear.co>
2021-12-26 23:42:53 -0500

    break down parser tests into smaller units

--- tests/parser_test_base.py
+++ tests/parser_test_base.py
@@ -4,7 +4,7 @@ from core.tokens import Token
 
 import unittest
 
-from typing import Iterator, List
+from typing import Iterable, Iterator
 
 
 parser = Parser()
@@ -14,19 +14,19 @@ class ParserTestBase(unittest.TestCase):
     def givenTokens(self, tokens: Iterator[Token]):
         self.tokens = tokens
 
-    def givenAbstractSyntaxTree(self, ast: Node):
+    def givenNode(self, ast: Node):
         self.ast = ast
 
     def whenParse(self):
         self.ast = parser(self.tokens)
 
-    def whenAsLines(self):
+    def whenNodeAsLines(self):
         self.lines = self.ast.as_lines()
         if isinstance(self.lines, Iterator):
             self.lines = list(self.lines)
 
-    def thenReturnAbstractSyntaxTree(self, expected_ast: dict):
-        self.assertEqual(expected_ast, self.ast.as_dict())
+    def thenReturnNode(self, expected_ast: Node):
+        self.assertEqual(expected_ast.as_dict(), self.ast.as_dict())
 
-    def thenReturnLines(self, expected_lines: List[str]):
+    def thenReturnLines(self, expected_lines: Iterable[str]):
         self.assertEqual(expected_lines, self.lines)
--- tests/test_parser_statement.py
+++ tests/test_parser_statement.py
@@ -1,4 +1,4 @@
-from core.ast import NodeType
+from core.ast import Node, NodeType
 from core.tokens import Identifier, Keyword, Literal, Operator, Symbol, Terminal, Token
 from tests.parser_test_base import ParserTestBase
 
@@ -10,17 +10,15 @@ class TestParserStatementSemicolon(ParserTestBase):
             Token(2, 1, Terminal.TERMINAL.label, None),
         ])
         self.whenParse()
-        self.thenReturnAbstractSyntaxTree(dict(
-            type=NodeType.SEQUENCE.label,
+        self.thenReturnNode(Node(
+            type=NodeType.SEQUENCE,
         ))
     
     def test_semicolon_as_lines(self):
-        self.givenTokens([
-            Token(1, 1, Symbol.SEMICOLON.label, None),
-            Token(2, 1, Terminal.TERMINAL.label, None),
-        ])
-        self.whenParse()
-        self.whenAsLines()
+        self.givenNode(Node(
+            type=NodeType.SEQUENCE,
+        ))
+        self.whenNodeAsLines()
         self.thenReturnLines([
             NodeType.SEQUENCE.label,
             ';',
@@ -38,16 +36,16 @@ class TestParserStatementAssignment(ParserTestBase):
             Token(2, 1, Terminal.TERMINAL.label, None),
         ])
         self.whenParse()
-        self.thenReturnAbstractSyntaxTree(dict(
-            type=NodeType.SEQUENCE.label,
-            left=dict(
-                type=NodeType.ASSIGN.label,
-                left=dict(
-                    type=NodeType.IDENTIFIER.label,
+        self.thenReturnNode(Node(
+            type=NodeType.SEQUENCE,
+            left=Node(
+                type=NodeType.ASSIGN,
+                left=Node(
+                    type=NodeType.IDENTIFIER,
                     value='abc',
                 ),
-                right=dict(
-                    type=NodeType.INT.label,
+                right=Node(
+                    type=NodeType.INT,
                     value='123',
                 ),
             ),
@@ -62,16 +60,16 @@ class TestParserStatementAssignment(ParserTestBase):
             Token(2, 1, Terminal.TERMINAL.label, None),
         ])
         self.whenParse()
-        self.thenReturnAbstractSyntaxTree(dict(
-            type=NodeType.SEQUENCE.label,
-            left=dict(
-                type=NodeType.ASSIGN.label,
-                left=dict(
-                    type=NodeType.IDENTIFIER.label,
+        self.thenReturnNode(Node(
+            type=NodeType.SEQUENCE,
+            left=Node(
+                type=NodeType.ASSIGN,
+                left=Node(
+                    type=NodeType.IDENTIFIER,
                     value='abc',
                 ),
-                right=dict(
-                    type=NodeType.IDENTIFIER.label,
+                right=Node(
+                    type=NodeType.IDENTIFIER,
                     value='xyz',
                 ),
             ),
@@ -89,28 +87,29 @@ class TestParserStatementPrintCharacter(ParserTestBase):
             Token(2, 1, Terminal.TERMINAL.label, None),
         ])
         self.whenParse()
-        self.thenReturnAbstractSyntaxTree(dict(
-            type=NodeType.SEQUENCE.label,
-            left=dict(
-                type=NodeType.PRINT_CHARACTER.label,
-                left=dict(
-                    type=NodeType.INT.label,
+        self.thenReturnNode(Node(
+            type=NodeType.SEQUENCE,
+            left=Node(
+                type=NodeType.PRINT_CHARACTER,
+                left=Node(
+                    type=NodeType.INT,
                     value='123',
                 ),
             ),
         ))
 
     def test_print_character_integer_as_lines(self):
-        self.givenTokens([
-            Token(1, 1, Keyword.PRINT_CHARACTER.label, None),
-            Token(1, 5, Symbol.OPEN_PARENTHESIS.label, None),
-            Token(1, 6, Literal.INT.label, '123'),
-            Token(1, 9, Symbol.CLOSE_PARENTHESIS.label, None),
-            Token(1, 10, Symbol.SEMICOLON.label, None),
-            Token(2, 1, Terminal.TERMINAL.label, None),
-        ])
-        self.whenParse()
-        self.whenAsLines()
+        self.givenNode(Node(
+            type=NodeType.SEQUENCE,
+            left=Node(
+                type=NodeType.PRINT_CHARACTER,
+                left=Node(
+                    type=NodeType.INT,
+                    value='123',
+                ),
+            ),
+        ))
+        self.whenNodeAsLines()
         self.thenReturnLines([
             NodeType.SEQUENCE.label,
             NodeType.PRINT_CHARACTER.label,
